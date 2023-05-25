10263c26c0558b04eeefd3756305a7389dd1eb0d
DohyeongAn <rrgg0204@naver.com>
2023-05-25 16:51:58 +0900

    0525

---
+++ src/commycompany/study/ch02/TrBoolean.java
@@ -0,0 +1,18 @@
+package commycompany.study.ch02;
+
+public class TrBoolean {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean defaultNy = true;
+		
+		if(defaultNy) {
+			System.out.println("참 ");
+		}else{System.out.println("거짓 ");}
+		
+		
+		
+	}
+
+}
