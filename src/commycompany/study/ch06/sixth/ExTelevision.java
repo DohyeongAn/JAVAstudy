1e0e84712c69a50aad65e716de489eb217ef31bd
DohyeongAn <rrgg0204@naver.com>
2023-06-12 17:32:43 +0900

    0612

---
+++ src/commycompany/study/ch06/sixth/ExTelevision.java
@@ -0,0 +1,12 @@
+package commycompany.study.ch06.sixth;
+
+public class ExTelevision {
+    public static void main(String[] args) {
+        System.out.println(Television.company);
+        System.out.println(Television.model);
+        System.out.println(Television.companyModel);
+        System.out.println(Television.info);
+        Television.abc();
+        Television.efg();
+    }
+}
---
+++ src/commycompany/study/ch06/trgugu/TrGugu.java
@@ -0,0 +1,14 @@
+package commycompany.study.ch06.trgugu;
+
+public class TrGugu {
+    public void printGugudan(int dan) {
+        System.out.println(dan + "단");
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(dan + " x " + i + " = " + (dan * i));
+        }
+        System.out.println();
+    }
+
+    }
+
+
